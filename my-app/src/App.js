60ccce2885edd18a0022d822f27de562b955ec4d
ahsunkh <ahsun45@gmail.com>
2021-08-26 12:06:10 +0500

    initial

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello World via React</h1>
+    </div>
+  );
+}
+
+export default App;
